a6df398b1ee72fae12fda8350b95d355b8c2f0fb
Chris Brown <chris.brown@booobiz.com>
2021-09-21 15:17:15 +0100

    Comments

--- .config/polybar/modules/bspwm.sh
+++ .config/polybar/modules/bspwm.sh
@@ -1,21 +1,26 @@
+#Binary Space Partition Window Module Definition
+##Basically the pretty workspaces part of the toolbar
 [module/bspwm]
+
+#Type of Module
 type = internal/bspwm
 
+#Labels for Active Workspace
 label-focused = %index%
 label-focused-background = ${colors.background-alt}
 label-focused-underline= ${colors.primary}
 label-focused-padding = 2
 
+#Labels for Workspaces which are live
 label-occupied = %index%
 label-occupied-padding = 2
 
+#Labels for Urgent Workspaces
 label-urgent = %index%!
 label-urgent-background = ${colors.alert}
 label-urgent-padding = 2
 
+#Labels for Empty Workspaces
 label-empty = %index%
 label-empty-foreground = ${colors.foreground-alt}
 label-empty-padding = 2
-
-; Separator in between workspaces
-; label-separator = |
