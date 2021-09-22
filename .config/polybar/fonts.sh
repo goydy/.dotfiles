061ff2990b8bc0e37e7efa3f9c00d1cc23cc6f7b
Chris Brown <chris.brown@booobiz.com>
2021-09-22 15:33:36 +0100

    Load Font Awesome for Polybar workspace module

--- .config/polybar/fonts.sh
+++ .config/polybar/fonts.sh
@@ -1,1 +1,5 @@
-font-0 = Source Code Pro:size:size=8;3
+font-0 = Source Code Pro:size:size=8
+font-1= Font Awesome 5 Free:pixelsize=8:antialias=true
+font-2 = Font Awesome 5 Free Solid:pixelsize=8:antialias=true
+font-3 = Font Awesome 5 Brands:pixelsize=8:antialias=true
+font-4 = Font Awesome 5 Free Solid:pixelsize=10:antialias=true
