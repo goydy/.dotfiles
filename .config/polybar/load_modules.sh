0448194a8957f731ee04e8715ac13774bb37980f
cbrown <cbrown@test.com>
2022-04-19 13:30:12 +0100

    Added date and time to centre of polybar

--- .config/polybar/load_modules.sh
+++ .config/polybar/load_modules.sh
@@ -1,3 +1,4 @@
 include-file = ~/.config/polybar/modules/bspwm.sh
 include-file = ~/.config/polybar/modules/battery.sh
 include-file = ~/.config/polybar/modules/wifi.sh
+include-file = ~/.config/polybar/modules/datetime.sh
---
+++ .config/polybar/modules/datetime.sh
@@ -0,0 +1,8 @@
+[module/datetime]
+type = internal/date
+interval = 1.0
+date = %A %d %B %Y
+time = %X
+label = %date% %time%
+format = 🕓 <label>
+format-foreground = #fff
