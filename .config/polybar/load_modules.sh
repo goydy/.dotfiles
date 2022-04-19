5ddbf098476fc9a0ff7aa7dfebd7f5606b95763c
cbrown <cbrown@test.com>
2022-04-19 11:24:09 +0100

    Add WiFi info to PolyBar.

--- .config/polybar/load_modules.sh
+++ .config/polybar/load_modules.sh
@@ -1,2 +1,3 @@
 include-file = ~/.config/polybar/modules/bspwm.sh
 include-file = ~/.config/polybar/modules/battery.sh
+include-file = ~/.config/polybar/modules/wifi.sh
---
+++ .config/polybar/modules/wifi.sh
@@ -0,0 +1,8 @@
+[module/wifi]
+type = internal/network
+interface = wlan0
+interval = 1
+ping-interval = 3
+udspeed-minwidth = 5
+accumulate-stats = false
+label-connected = 💩 %netspeed%
