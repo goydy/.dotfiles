607008e376822b32f0d34f47b1b60baffe27a5f2
cbrown <cbrown@test.com>
2022-04-20 18:43:26 +0100

    Changed volume back to %, dB was pissing me off with 0 being 100% volume

--- .config/polybar/modules/volume.sh
+++ .config/polybar/modules/volume.sh
@@ -1,11 +1,8 @@
 [module/volume]
-type = internal/pulseaudio
-sink = alsa_output.pci-0000_00_1b.0.analog-stereo
-
-label-volume = %decibels%
-label-muted = Volume 0dB
-format-volume = Volume <label-volume>dB
-interval = 5
-
 click-right = pavucontrol
-
+format-volume = Volume <label-volume>%
+interval = 5
+label-muted = Volume Muted!
+label-volume = %percentage%
+sink = alsa_output.pci-0000_00_1b.0.analog-stereo
+type = internal/pulseaudio
