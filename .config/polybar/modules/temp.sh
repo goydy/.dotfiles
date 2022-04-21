3ae3169e3b2e3afc697166fa0d2970a74073fa8c
cbrown <cbrown@test.com>
2022-04-21 19:49:03 +0100

    Changed temp input device

--- .config/polybar/modules/temp.sh
+++ .config/polybar/modules/temp.sh
@@ -3,7 +3,7 @@ type = internal/temperature
 
 interval = 1
 thermal-zone = 0
-hwmon-path = /sys/devices/platform/coretemp.0/hwmon/hwmon4/temp1_input
+hwmon-path = /sys/devices/virtual/thermal/thermal_zone0/hwmon1/temp1_input
 base-temperature = 0
 warn-temperature = 75
 label = %temperature-c%
