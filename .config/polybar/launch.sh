eac833606e1af23d8f08ac92dabb28b5c290ac5e
cbrown <cbrown@test.com>
2022-07-11 16:09:05 +0100

    Added Multiple Monitor Detection Scripts

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -3,4 +3,14 @@
 killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-polybar main &
+#Determine if there is an external Monitor Connected
+if [[ $(xrandr -q | grep -w  "HDMI1 connected") ]]; 
+then
+	
+	polybar hdmi-main &
+	polybar hdmi-not-main &
+else
+
+	polybar undocked-main &
+
+fi
